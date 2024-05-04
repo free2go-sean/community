62aba9fb3a67d7be1e474764852f57cee43b21ab
sean <free2go.sean@gmail.com>
2024-05-04 16:16:56 +0900

    project init

---
+++ src/test/java/com/free2go/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free2go.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
