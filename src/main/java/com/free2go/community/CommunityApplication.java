6b296600892acce19bf3932a48f6fed9fd3720df
jeondayoung <jdt1025@naver.com>
2024-05-06 14:12:13 +0900

    test

--- src/main/java/com/free2go/community/CommunityApplication.java
+++ src/main/java/com/free2go/community/CommunityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class CommunityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
     }
